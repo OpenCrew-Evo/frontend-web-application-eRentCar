176aaf63503c696a26f0e2ef6899c84abac1b6db
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-06-07 21:17:00 -0500

    feat: refactored user register, close #10

--- src/app/freeviews/pages/register/register.component.ts
+++ src/app/freeviews/pages/register/register.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ClientService} from "../../../my-profile/services/client.service";
 import {MatStepper} from "@angular/material/stepper";
-import { v4 as uuid } from 'uuid';
-import {Client} from "../../../my-profile/model/client";
 import {AuthService} from "../../../api/auth.service";
 
 @Component({
@@ -15,14 +13,11 @@ export class RegisterComponent implements OnInit {
   showPassword: Boolean = false;
   userNameAndPasswordForm: FormGroup;
   personalInformationForm: FormGroup;
-  isSuccessful = false;
-  isSignUpFailed = false;
   errorMessage = '';
 
-    constructor(private formBuilder: FormBuilder,
-      private clientService: ClientService,
-      private authService: AuthService
-  ) {
+  constructor(private formBuilder: FormBuilder,
+              private clientService: ClientService,
+              private authService: AuthService) {
       this.userNameAndPasswordForm = this.formBuilder.group({
         userName: ['', [Validators.required]],
         email: ['', [Validators.required, Validators.pattern("^(([^<>()[\\]\\\\.,;:\\s@\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$")]],
@@ -33,12 +28,12 @@ export class RegisterComponent implements OnInit {
         names: ["", [Validators.required, Validators.maxLength(30)]],
         lastNames: ["", [Validators.required, Validators.maxLength(30)]],
         address: ["", [Validators.required, Validators.maxLength(50)]],
-        cellphoneNumber: ["", [Validators.required, Validators.pattern("^(9)([0-9]){8}$")]]
+        cellphoneNumber: ["", [Validators.required, Validators.pattern("^(9)([0-9]){8}$")]],
+        pathImageProfile: ["", [Validators.required]]
       });
-    }
+  }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {}
 
   validateUser(stepper: MatStepper) {
     this.authService.register(
@@ -67,10 +62,10 @@ export class RegisterComponent implements OnInit {
       lastNames: this.personalInformationForm.value.lastNames,
       address: this.personalInformationForm.value.address,
       cellphoneNumber: this.personalInformationForm.value.cellphoneNumber,
+      imagePath: this.personalInformationForm.value.pathImageProfile,
       averageResponsibility: -1,
       responseTime: -1,
       rate: -1,
-      imagePath: "",
       userId: JSON.parse(<string>localStorage.getItem("userId"))
     }
 
